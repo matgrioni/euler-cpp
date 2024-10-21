df2bc0bfb7e9635c26d2576aba191e0d3ff5e4da
Matias Grioni <matgrioni@gmail.com>
2024-10-21 15:48:08 -0300

    WIP.

---
+++ lib/inc/Math.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+namespace euler
+{
+	template <typename T>
+	constexpr T Exp(const T& p_base, const T& p_power)
+	{
+		if (p_power == 0)
+		{
+			return 1;
+		}
+
+		if (p_power == 1)
+		{
+			return p_base;
+		}
+
+		auto result = p_base;
+		for (T i = 0; i < p_power - 1; ++i)
+		{
+			result *= p_base;
+		}
+
+		return result;
+	}
+}
